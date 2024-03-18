a1de4a9bb8d94d7454bb8abc624f85686cb8e601
Shailendratechdom <138393368+Shailendratechdom@users.noreply.github.com>
2024-03-18 16:29:22 +0530

    feat:forgot password feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #Test Imports
-from .views import GetDataView, GetIcompanyId, LoginApi, apipage
+from .views import *
 
 # Working Imports
 from .views import Dashboard
@@ -12,14 +12,20 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     #Required 
-    path('login/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('settings/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 
     #Login and Get Server
     path('', apipage, name='apipage'),
-    path('geticomany', GetIcompanyId.as_view(), name='geticomany'),
-    path('login', CustomUserLoginView.as_view(), name='user-login'),
-    path('login-view', LoginApi.as_view(), name='login-view'),
-    path('getdata', GetDataView.as_view(), name='get_data'),
+    path('settings/geticomany', GetIcompanyId.as_view(), name='geticomany'),
+    path('settings/login', CustomUserLoginView.as_view(), name='user-login'),
+    path('settings/login-view', LoginApi.as_view(), name='login-view'),
+    path('settings/settingsgetdata', GetDataView.as_view(), name='get_data'),
+    
+    
+    path('settings/forgot-otp/', ForgotPasswordOTPView.as_view(), name='forgot-otp'),
+    path('settings/verify-forgot-otp/', VerifyForgotPasswordOTPView.as_view(), name='verify-forgot-otp'),
+    # path('settings/update-password/', UpdatePasswordView.as_view(), name='update-password'),
+    
 
     #After Login - Home Page API
     path('dashboard', Dashboard.as_view(), name='dashboard'),
--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,15 @@ from django.contrib.auth import authenticate
 from django.db import connection, connections
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-from .models import CustomUser
+from .models import CustomUser,OTP
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from django.views.decorators.csrf import csrf_exempt
+import sentry_sdk
+from django.template.loader import render_to_string
+import random
+import re
+import uuid
 #Private methods
 
 from .encodedDbs import encode_string,decode_string
@@ -285,5 +293,156 @@ class GetIcompanyId(APIView):
 def apipage(request):
     friends=['API Accounts Server Running ...']
     return JsonResponse(friends,safe=False)
+
+
+
+class ForgotPasswordOTPView(APIView):
+    """
+    View to send the OTP For Forget Password via email and update it in the database.
+    This endpoint allows you to send the latest OTP via email and update it in the database.
+
+    :param email: Recipient's email address.
+    """
+
+    @swagger_auto_schema(
+        operation_summary="OTP for forgot or reset password.",
+        operation_description="OTP for forgot or reset password.",
+        manual_parameters=[openapi.Parameter('email', openapi.IN_QUERY, description="Recipient's email address",
+                                             type=openapi.TYPE_STRING)],
+        responses={
+            200: 'OTP sent successfully',
+            400: 'Bad Request - Email is required or no OTP exists for the email',
+            500: 'Internal Server Error - Failed to send OTP via email',
+        }
+    )
+    @csrf_exempt
+    def post(self, request, format=None):
+        """
+        Send the latest OTP via email and update it in the database.
+
+        :param request: The HTTP request object.
+        :param format: The format of the response (e.g., JSON).
+        :return: Returns a success message if the latest OTP is sent and updated successfully.
+                 Returns an error message if no OTP exists for the email or if any issues occur.
+        """
+        sentry_sdk.capture_message("ForgotPasswordOTPView")
+        email = request.query_params.get('email')
+
+        if not email:
+            return Response({'error': 'Email ID is mandatory to be entered. Please enter the email ID.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if the email exists in the User table
+        try:
+            user = CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return Response({
+                'error': 'Email ID entered by you is not registered with us. Please contact our support team for assistance.'},
+                status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            otp = OTP.objects.filter(
+                email=email, expired=False).latest('created_at')
+        except OTP.DoesNotExist:
+            otp_code = ''.join([str(random.randint(0, 9)) for _ in range(6)])
+            otp = OTP(email=email, code=otp_code)
+        # Generate a new 6-digit OTP
+        otp_code = ''.join([str(random.randint(0, 9)) for _ in range(6)])
+
+        subject = 'OTP to Reset Password'
+        message = render_to_string('settingapp_forgotpassword_otp.html',
+                                   {'otp_code': otp_code, 'first_name': user.first_name.capitalize(),
+                                    'last_name': user.last_name.capitalize()})
+
+        try:
+            # zeptomail_send_email(subject, content=message, from_email="", to_email=[email])
+
+            otp.code = otp_code
+            otp.save()
+            return Response({'message': 'OTP sent successfully.'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            error_message = f"Failed to send the OTP via email. Please try after sometime."
+            return Response({'error': error_message}, status=status.HTTP_429_TOO_MANY_REQUESTS)
+
+
+class VerifyForgotPasswordOTPView(APIView):
+    """
+    View to verify the OTP code for password reset.
+
+    This endpoint allows you to verify the OTP code provided by the user for password reset.
+    If the OTP is valid and not expired, it is marked as expired, and the verification is successful.
+
+    :param email: The user's email for which OTP is being verified.
+    :param otp_code: The OTP code provided by the user.
+    """
+
+    @swagger_auto_schema(
+        operation_summary="Verify the Forget/Reset Password OTP Code.",
+        operation_description="Verify the Forget/Reset Password OTP Code.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'email': openapi.Schema(type=openapi.TYPE_STRING, description="User's email"),
+                'otp_code': openapi.Schema(type=openapi.TYPE_STRING, description="OTP code"),
+            },
+            required=['email', 'otp_code']
+        ),
+        responses={
+            200: 'OTP verification successful',
+            400: 'Invalid OTP code or OTP has expired',
+        }
+    )
+    @csrf_exempt
+    def post(self, request, format=None):
+        """
+        Verify the OTP code provided by the user for password reset.
+
+        :param request: The HTTP request object.
+        :param format: The format of the response (e.g., JSON).
+        :return: Returns a success message if OTP verification is successful.
+                 Returns an error message if the OTP is invalid or has expired.
+        """
+        sentry_sdk.capture_message("VerifyForgotPasswordOTPView")
+        email = request.data.get('email')
+        otp_code = request.data.get('otp_code')
+
+        if not email:
+            return Response({'error': 'Email ID is mandatory to be entered. Please enter the email ID.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if not otp_code:
+            return Response({'error': 'OTP is mandatory to be entered. Please enter the OTP.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return Response({
+                'error': 'Email ID entered by you is not registered with us. Please contact our support team for assistance.'},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            otp = OTP.objects.get(email=email, code=otp_code, expired=False)
+        except OTP.DoesNotExist:
+            return Response({'error': 'Invalid OTP. Please enter a valid OTP sent to your email.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if otp.is_expired():
+            return Response({'error': 'OTP has expired. Please use Resend OTP to send a new OTP.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # OTP is valid
+        otp.expired = True
+        otp.save()
+        token = RefreshToken.for_user(user)
+
+        return Response(
+            {'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email, 'token': token,
+             'msg': 'OTP verification successful'},
+            status=status.HTTP_200_OK)
+        
+
+
 
 
