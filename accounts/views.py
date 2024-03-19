e45bb81dd54fc96c5f37ed79be82443bb3822289
anukulRST <anuku@renukasoftec.com>
2024-03-19 17:47:27 +0530

    sandgrid changes

--- accounts/views.py
+++ accounts/views.py
@@ -24,11 +24,12 @@ from rest_framework import status
 from accounts.utils.sendgrid_mail import *
 from django.core.exceptions import ValidationError
 from django.contrib.auth.hashers import make_password, check_password
+import os
 #Private methods
 
 from .encodedDbs import encode_string,decode_string
 
-
+SENDGRID_EMAIL = os.environ.get('SENDGRID_EMAIL')
 
 class GetIcompanyId(APIView):
     """
@@ -382,7 +383,7 @@ class ForgotPasswordOTPView(APIView):
                                     'last_name': user.username.capitalize()})
 
         try:
-            sendgrid_send_mail(subject, content=message, from_email="shailendra.tiwari@infovision.com", to_email=email)
+            sendgrid_send_mail(subject, content=message, from_email=SENDGRID_EMAIL, to_email=email)
 
             otp.code = otp_code
             otp.save()
@@ -540,7 +541,7 @@ class UpdatePasswordView(APIView):
             message = render_to_string('settingapp_reset_successful.html',
                                        {'first_name': user.first_name.capitalize(), 'last_name': user.last_name.capitalize()})
             
-            sendgrid_send_mail(subject=subject, content=message, from_email="shailendra.tiwari@infovision.com", to_email=[email])
+            sendgrid_send_mail(subject=subject, content=message, from_email=SENDGRID_EMAIL, to_email=[email])
             
             user.password = make_password(new_password)
             user.save()
