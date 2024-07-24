3aee1e067deabd318add0ad1157fefa3fda7129a
RajatDhiman085 <dhimanrajat085@gmail.com>
2024-07-24 13:29:25 +0530

    rd

--- RenukaSoft/urls.py
+++ RenukaSoft/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     path("api/v1/",include('accounts.urls')),
     path("api/v1/qm",include('estimation.urls')),
     path("api/v1/om",include('ordermanagement.urls')),
-
+    path("api/v1/Gen",include('GeneralAPIs.urls')),
 
 
     
