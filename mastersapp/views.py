724a324c0675092026016b39b553d7db124c0a8e
RajatDhiman085 <dhimanrajat085@gmail.com>
2024-08-09 12:49:56 +0530

    Minor change in TextMatter Checking API

--- mastersapp/views.py
+++ mastersapp/views.py
@@ -103,7 +103,13 @@ class TextMatterCheckingViewSet(viewsets.ModelViewSet):
         tags=['Text Matter Checking']
     )
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        response_data = {
+            "message": "Success",
+            "data": serializer.data
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
 
     @swagger_auto_schema(
         operation_summary="Retrieve a Text Matter Checking",
@@ -120,7 +126,13 @@ class TextMatterCheckingViewSet(viewsets.ModelViewSet):
         tags=['Text Matter Checking']
     )
     def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        response_data = {
+            "message": "Success",
+            "data": serializer.data
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
 
     @swagger_auto_schema(
         operation_summary="Create a Text Matter Checking",
@@ -130,7 +142,14 @@ class TextMatterCheckingViewSet(viewsets.ModelViewSet):
         tags=['Text Matter Checking']
     )
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        response_data = {
+            "message": "Success",
+            "data": serializer.data
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED)
 
     @swagger_auto_schema(
         operation_summary="Update a Text Matter Checking",
@@ -148,7 +167,16 @@ class TextMatterCheckingViewSet(viewsets.ModelViewSet):
         tags=['Text Matter Checking']
     )
     def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        response_data = {
+            "message": "Success",
+            "data": serializer.data
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
 
     @swagger_auto_schema(
         operation_summary="Partially Update a Text Matter Checking",
@@ -166,7 +194,7 @@ class TextMatterCheckingViewSet(viewsets.ModelViewSet):
         tags=['Text Matter Checking']
     )
     def partial_update(self, request, *args, **kwargs):
-        return super().partial_update(request, *args, **kwargs)
+        return self.update(request, *args, **kwargs, partial=True)
 
     @swagger_auto_schema(
         operation_summary="Delete a Text Matter Checking",
@@ -183,4 +211,10 @@ class TextMatterCheckingViewSet(viewsets.ModelViewSet):
         tags=['Text Matter Checking']
     )
     def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        response_data = {
+            "message": "Success",
+            "data": {}
+        }
+        return Response(response_data, status=status.HTTP_204_NO_CONTENT)
